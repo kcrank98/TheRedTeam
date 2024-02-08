9148a18af134cdb078a4e28e19b94d5ca796c0b6
BlakeWelch117 <126487654+BlakeWelch117@users.noreply.github.com>
2024-02-08 18:38:16 -0500

    B.W
    added pause ui with win loss menu functionality

---
+++ RedTeamGame/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()//resume the game from where it was paused if aplicable
+    {
+        gameManager.instance.stateUnPaused();//unpause
+    }
+    public void restart()//resets the current game
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);//reload curent scene
+        gameManager.instance.stateUnPaused();//unpause
+    }
+    public void quit()//closes application
+    {
+        Application.Quit();//self explanatory
+    }
+
+}
