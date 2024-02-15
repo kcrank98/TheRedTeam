26dbb9368b555f5225c941caa95b5a167e69cc3e
Evan Lapice <evan.lapice@outlook.com>
2024-02-14 20:59:31 -0500

    EL
    Weapon Rifle Back to Normal

--- RedTeamGame/Assets/Scripts/playerController.cs
+++ RedTeamGame/Assets/Scripts/playerController.cs
@@ -1,74 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerController : MonoBehaviour, IDamage
 {
     [SerializeField] CharacterController controller;
 
-    [SerializeField] int HP;
+    [Header("Movement")]
     [SerializeField] float playerSpeed;
     [SerializeField] int jumpMax;
     [SerializeField] float jumpForce;
     [SerializeField] float gravity;
+    int jumpCount;
 
+    [Header("Shooting")]
     [SerializeField] int shootDamage;
     [SerializeField] int shootDistance;
     [SerializeField] float shootRate;
-    [SerializeField] float HPPerc;
 
-    [SerializeField] private gunSystem gunSystem;
-    [SerializeField] private gunSystem.GunProperties currentGun;
+    [Header("HP")]
+    [SerializeField] float HPPerc;
+    [SerializeField] int HP;
+    [SerializeField] int HPOrig;
+
+
+    [HideInInspector][SerializeField] gun currentGun;
+    [HideInInspector][SerializeField] int currentGunDamage;
+
+    [HideInInspector][SerializeField] Transform shootPos;
+    [HideInInspector][SerializeField] GameObject bullet;
+
+    // test code
+    //public static Action shootInput;
 
     Vector3 move;
     Vector3 playerVel;
-    int jumpCount;
     bool isShooting;
-    int HPOrig;
 
     // Start is called before the first frame update
     void Start()
     {
         HPOrig = HP;
         respawn();
-        //gunSystem = new gunSystem();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if (!gameManager.instance.isPaused)
         {
-            //Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.blue);
-
             movement();
-
-            float scrollWheelInput = Input.GetAxis("Mouse ScrollWheel");
-
-            //Change gun based on scroll wheel movement
-            if (scrollWheelInput > 0f) // Scroll up
-            {
-                gunSystem.SwitchGun(gunSystem.GunType.Pistol);
-                currentGun = gunSystem.GetCurrentGun();
-
-
-            }
-            else if (scrollWheelInput <= 0f) // Scroll down
-            {
-                gunSystem.SwitchGun(gunSystem.GunType.Rifle);
-                currentGun = gunSystem.GetCurrentGun();
-
-            }
-
-            gunSystem.GunProperties gun = gunSystem.GetCurrentGun();
 
             if (Input.GetButton("Shoot") && !isShooting)
             {
                 StartCoroutine(shoot());
             }
         }
-
     }
     private void movement()
     {
@@ -99,15 +88,13 @@ public class playerController : MonoBehaviour, IDamage
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDistance))
         {
-            //Debug.Log(hit.collider.name);
+            Debug.Log(hit.collider.name);
 
             IDamage dmg = hit.collider.GetComponent<IDamage>();
 
             if (hit.transform != transform && dmg != null)
             {
-                shootDamage = currentGun.shootDamage;
-                dmg.takeDamage(shootDamage);
-                //dmg.takeDamage(currentGun.shootDamage);  
+                dmg.takeDamage(shootDamage);       
             }
         }
 
@@ -120,7 +107,7 @@ public class playerController : MonoBehaviour, IDamage
         HP -= amount;
         
         StartCoroutine(flashDamage());
-        //checkHPBelowPerc();
+        checkHPBelowPerc();
 
         if (HP <= 0)
         {
@@ -129,33 +116,28 @@ public class playerController : MonoBehaviour, IDamage
     }
 
 
-    //void checkHPBelowPerc()
-    //{
-    //    if (HP <= HPOrig * HPPerc)
-    //    {
-    //        //gameManager.instance.damagePersist.gameObject.SetActive(true);
-    //        gameManager.instance.damagePersist.enabled = true;
-    //    }
-    //    else
-    //    {
-    //        //gameManager.instance.damagePersist.gameObject.SetActive(false);
-    //        gameManager.instance.damagePersist.enabled = false;
-
-    //    }
-    //}
+    void checkHPBelowPerc()
+    {
+        if (HP <= HPOrig * HPPerc)
+        {
+            gameManager.instance.damagePersist.gameObject.SetActive(true);
+        }
+        else
+        {
+            gameManager.instance.damagePersist.gameObject.SetActive(false);
+        }
+    }
 
     IEnumerator flashDamage()
     {
         gameManager.instance.damageFlash.gameObject.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         gameManager.instance.damageFlash.gameObject.SetActive(false);
-        //checkHPBelowPerc();
 
     }
     public void respawn()
     {
         HP = HPOrig;
-        //checkHPBelowPerc();
 
         controller.enabled = false;
         transform.position = gameManager.instance.playerSpawnPos.transform.position;
