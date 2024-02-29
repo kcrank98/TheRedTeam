fc523b4d5e0d44f28c59a291a289b5cb7ca1de73
kcrank98 <138240807+kcrank98@users.noreply.github.com>
2024-02-28 19:37:37 -0500

    KC
    added spawner

---
+++ RedTeamGame/Assets/Scripts/spawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int numberToSpawn;
+    [SerializeField] int spawnTimer;
+    [SerializeField] Transform[] spawnPos;
+
+    int spawnCount;
+    bool isSpawning;
+    bool startSpawning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.instance.updateGameGoal(numberToSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startSpawning && !isSpawning && spawnCount < numberToSpawn)
+        {
+            StartCoroutine(spawn());
+        }
+    }
+
+    IEnumerator spawn()
+    {
+        isSpawning = true;
+
+        int arrayPos = Random.Range(0, spawnPos.Length);
+
+        Instantiate(objectToSpawn, spawnPos[arrayPos].position, spawnPos[arrayPos].rotation);
+        spawnCount++;
+        yield return new WaitForSeconds(spawnTimer);
+
+        isSpawning = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+}
